1e9e83186eb8160de4bf4f5c9c7baab7df38e777
Lee Sang Hyuk <desummit1@gmail.com>
2020-10-18 00:47:06 +0900

    Build#2: Add Handpose
    HandPose가 0.1초마다 웹캠을 통해서 손을 인식한다.(손마디 포함)

--- beforeResult.js
+++ beforeResult.js
@@ -1,14 +1,59 @@
 const handpose = require("@tensorflow-models/handpose");
-const tf = require("@tensorflow/tfjs");
+const tf = require("@tensorflow/tfjs-core");
+require("@tensorflow/tfjs-backend-webgl");
 
-let video = document.querySelector("#videoElement");
+const video = document.getElementById("videoElement");
+const canvas = document.getElementById("handPrint");
 
-if(navigator.mediaDevices.getUserMedia){
-    navigator.mediaDevices.getUserMedia({video:'true'})
-    .then((stream) => {
-        video.srcObject = stream;
-    })
-    .catch((err)=>{
-        console.log("Error from webcam! Need fix!");
-    });
+const state = {
+    backend: "webgl",
+};
+
+const ctx = canvas.getContext('2d');
+let checkCameraActive;
+
+navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+const findHand = async (model) => {
+    if(checkCameraActive){
+        const videoWidth = video.videoWidth;
+        const videoHeight = video.videoHeight;
+  
+        canvas.width = videoWidth;
+        canvas.height = videoHeight;
+  
+        video.width = videoWidth;
+        video.height = videoHeight;
+  
+        const hand = await model.estimateHands(video);
+        console.log(hand);
+    }
 }
+
+const loadHandpose = async () => {
+    const model = await handpose.load();
+    runCamera();
+    console.log("Loaded HandPose");
+    setInterval(() => {
+        findHand(model);
+    }, 100);
+}
+
+const runCamera = () => {
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({faceingMode: "user", audio:false, video:{width: 500, height: 375}})
+        .then((stream) => {
+            video.srcObject = stream;
+            checkCameraActive = 'true'
+        })
+        .catch((err)=>{
+            checkCameraActive = 'false'
+            console.log(`Error from webcam! Need fix! : ${err}`);
+        });
+    }
+}
+
+loadHandpose();
