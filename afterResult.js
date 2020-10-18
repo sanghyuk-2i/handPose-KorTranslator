20a31604cb5481794353e85a62edc072334359e8
Lee Sang Hyuk <desummit1@gmail.com>
2020-10-18 17:41:25 +0900

    Build#3: Hand Point
    손가락마다 점(마디)를 표현할 수 있게 만들었음

--- afterResult.js
+++ afterResult.js
@@ -6,17 +6,42 @@ require("@tensorflow/tfjs-backend-webgl");
 const video = document.getElementById("videoElement");
 const canvas = document.getElementById("handPrint");
 
-const state = {
-    backend: "webgl",
-};
-
-const ctx = canvas.getContext('2d');
-let checkCameraActive;
+let checkCameraActive,
+    fingerPoint = {
+        thumb: [0, 1, 2, 3, 4],
+        indexFinger: [0, 5, 6, 7, 8],
+        middleFinger: [0, 9, 10, 11, 12],
+        ringFinger: [0, 13, 14, 15, 16],
+        pinky: [0, 17, 18, 19, 20],
+    };
 
 navigator.getUserMedia =
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia;
+
+const runCamera = () => {
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({faceingMode: "user", audio:false, video:{width: 500, height: 375}})
+        .then((stream) => {
+            video.srcObject = stream;
+            checkCameraActive = 'true'
+        })
+        .catch((err)=>{
+            checkCameraActive = 'false'
+            console.log(`Error from webcam! Need fix! : ${err}`);
+        });
+    }
+}
+
+const loadHandpose = async () => {
+    const model = await handpose.load();
+    runCamera();
+    console.log("Loaded HandPose");
+    setInterval(() => {
+        findHand(model);
+    }, 100);
+}
 
 const findHand = async (model) => {
     if(checkCameraActive){
@@ -31,29 +56,48 @@ const findHand = async (model) => {
   
         const hand = await model.estimateHands(video);
         console.log(hand);
+
+        const ctx = canvas.getContext('2d');
+        canvasHandPoint(ctx, hand);
     }
 }
 
-const loadHandpose = async () => {
-    const model = await handpose.load();
-    runCamera();
-    console.log("Loaded HandPose");
-    setInterval(() => {
-        findHand(model);
-    }, 100);
+const canvasHandPoint = (ctx, prediction) => {
+    if(prediction.length > 0){
+        prediction.forEach((predict) => {
+            const landmark = predict.landmarks;
+            fillHandPoint(ctx, landmark);
+            fillHandLine(ctx, landmark);
+        })
+    }
 }
 
-const runCamera = () => {
-    if(navigator.mediaDevices.getUserMedia){
-        navigator.mediaDevices.getUserMedia({faceingMode: "user", audio:false, video:{width: 500, height: 375}})
-        .then((stream) => {
-            video.srcObject = stream;
-            checkCameraActive = 'true'
-        })
-        .catch((err)=>{
-            checkCameraActive = 'false'
-            console.log(`Error from webcam! Need fix! : ${err}`);
-        });
+const fillHandPoint = (ctx, landmark) => {
+    for(let i = 0; i < landmark.length; i++){
+        const x = landmark[i][0];
+        const y = landmark[i][1];
+
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 3 * Math.PI);
+        ctx.fill();
+    }
+}
+
+const fillHandLine = (ctx, landmark) => {
+    let num = 0;
+    for (const handPoint of Object.keys(fingerPoint)) {
+        for(let i = 0; i < handPoint.length - 1; i++){
+            console.log(`After handpoint : ${handPoint}`);
+            const firstPoint = annotations[handPoint][i];
+            const secondPoint = annotations[handPoint][i + 1];
+
+            const region = new Path2D();
+            region.moveTo(landmark[firstPoint], landmark[firstPoint]);
+            region.lineTo(landmark[secondPoint], landmark[secondPoint]);
+            region.closePath();
+            ctx.stroke(region);
+        }
+        num += 1;
     }
 }
 
--- beforeResult.js
+++ beforeResult.js
@@ -5,17 +5,42 @@ require("@tensorflow/tfjs-backend-webgl");
 const video = document.getElementById("videoElement");
 const canvas = document.getElementById("handPrint");
 
-const state = {
-    backend: "webgl",
-};
-
-const ctx = canvas.getContext('2d');
-let checkCameraActive;
+let checkCameraActive,
+    fingerPoint = {
+        thumb: [0, 1, 2, 3, 4],
+        indexFinger: [0, 5, 6, 7, 8],
+        middleFinger: [0, 9, 10, 11, 12],
+        ringFinger: [0, 13, 14, 15, 16],
+        pinky: [0, 17, 18, 19, 20],
+    };
 
 navigator.getUserMedia =
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia;
+
+const runCamera = () => {
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({faceingMode: "user", audio:false, video:{width: 500, height: 375}})
+        .then((stream) => {
+            video.srcObject = stream;
+            checkCameraActive = 'true'
+        })
+        .catch((err)=>{
+            checkCameraActive = 'false'
+            console.log(`Error from webcam! Need fix! : ${err}`);
+        });
+    }
+}
+
+const loadHandpose = async () => {
+    const model = await handpose.load();
+    runCamera();
+    console.log("Loaded HandPose");
+    setInterval(() => {
+        findHand(model);
+    }, 100);
+}
 
 const findHand = async (model) => {
     if(checkCameraActive){
@@ -30,29 +55,29 @@ const findHand = async (model) => {
   
         const hand = await model.estimateHands(video);
         console.log(hand);
+
+        const ctx = canvas.getContext('2d');
+        canvasHandPoint(ctx, hand);
     }
 }
 
-const loadHandpose = async () => {
-    const model = await handpose.load();
-    runCamera();
-    console.log("Loaded HandPose");
-    setInterval(() => {
-        findHand(model);
-    }, 100);
+const canvasHandPoint = (ctx, prediction) => {
+    if(prediction.length > 0){
+        prediction.forEach((predict) => {
+            const landmark = predict.landmarks;
+            fillHandPoint(ctx, landmark);
+        })
+    }
 }
 
-const runCamera = () => {
-    if(navigator.mediaDevices.getUserMedia){
-        navigator.mediaDevices.getUserMedia({faceingMode: "user", audio:false, video:{width: 500, height: 375}})
-        .then((stream) => {
-            video.srcObject = stream;
-            checkCameraActive = 'true'
-        })
-        .catch((err)=>{
-            checkCameraActive = 'false'
-            console.log(`Error from webcam! Need fix! : ${err}`);
-        });
+const fillHandPoint = (ctx, landmark) => {
+    for(let i = 0; i < landmark.length; i++){
+        const x = landmark[i][0];
+        const y = landmark[i][1];
+
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 3 * Math.PI);
+        ctx.fill();
     }
 }
 
